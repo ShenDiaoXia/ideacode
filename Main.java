08275e5668996a60080b70dc04bd315419a7f1a4
shendiaoxia <804589742@qq.com>
2019-03-15 21:15:49 +0800

    初次提交一个项目

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 15;
+        int b = 20;
+        int d = a + b;
+        System.out.println(d);
+    }
+}
